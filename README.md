2dac6d2b26c743c807c4eee4548c2fdef8aeeec9
Ayan Sengupta <ayansengupta17@gmail.com>
2017-07-13 15:54:11 +0530

    first upload

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # auto_login
 This is a small code snippet, for auto login for guys staying in IIT Bombay.
+
+##Dependencies
+
+* Python 2.7
+* Selenium python library
+* PhantomJS (optional)
+* Chrome Webdriver
+
+I am against using PhantomJS with python, but anyway I added a line, so anyone can use it. The easiest way to use PhantomJS in python is via Selenium. The simplest installation method is
+
+* Install NodeJS
+* Using Node's package manager install phantomjs: npm -g install phantomjs-prebuilt
+* install selenium (in your virtualenv, if you are using that)
+
+You can use, any other webdriver like firefox webdriver.
+
+
+## How to use
+Create a file in which you will keep your LDAP username and the LDAP password, each in differnt lines. A sample 'key' file is already provided. Just run the code from the terminal.
+
+'''python
+python auto_login_ldap.py key
+'''
+
+This is done to keep your password safe in a different file. But you can put your password directly in the code for testing, but that won't be so much secured.
+
+
---
+++ auto_login_ldap.py
@@ -0,0 +1,61 @@
+"""Autologin Ldap quickly"""
+
+# all imported libraries
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+import argparse
+
+# parsing the file passed as argument
+
+parser = argparse.ArgumentParser()
+parser.add_argument('file', type=argparse.FileType('r'))
+args = parser.parse_args()
+with args.file as file:
+    content = file.readlines()
+content = [x.strip() for x in content]
+
+#username and password
+
+username = content[0] # Your LDAP username
+password = content[1] # Your LDAP passsword,
+
+# setting up the webdriver using chromedriver
+driver = webdriver.Chrome()
+driver.set_window_size(50, 50)
+driver.set_window_position(0, 0)
+# driver = webdriver.PhantomJS()
+driver.get("https://internet.iitb.ac.in/index.php")
+delay = 3
+
+# wait for the page to load
+
+try:
+    myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, '/html/body/table[1]/tbody/tr/td/table/tbody/tr/td[2]/font/h1')))
+    print "Page is ready!"
+except TimeoutException:
+    print "Loading took too much time!"
+    driver.close()
+
+# attempt logging in
+try:
+    ip = driver.find_element_by_xpath("/html/body/center/center[2]/div/table/tbody/tr/td[2]/center")
+    print("Already Logged in")
+except:
+    print("Attempting logging")
+    u = driver.find_element_by_xpath("/html/body/table/tbody/tr[1]/td/table/tbody/tr/td[3]/form/table/tbody/tr[2]/td[1]/input")
+    p = driver.find_element_by_xpath("/html/body/table/tbody/tr[1]/td/table/tbody/tr/td[3]/form/table/tbody/tr[2]/td[2]/input")
+    u.send_keys(username)
+    p.send_keys(password)
+    driver.find_element_by_xpath("/html/body/table/tbody/tr[1]/td/table/tbody/tr/td[3]/form/table/tbody/tr[2]/td[3]/input").click()
+    print "Login Sucessful"
+
+
+
+
+
+driver.close()
